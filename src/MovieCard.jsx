f17c7d44c1f0a9e435a8d2ef60087e4ebc10fbca
Jovin Valdez <jovinvaldez@berkeley.edu>
2024-07-30 16:52:29 -0700

    added movie card components to home page

---
+++ src/MovieCard.jsx
@@ -0,0 +1,14 @@
+// src/GeneralButton.js
+import React from 'react';
+import './MovieCard.css';
+
+const MovieCard = ({ }) => {
+  return (
+    <div className="movie-card">
+        <div className="movie-poster"></div>
+        <div>Movie title</div>
+    </div>
+  );
+};
+
+export default MovieCard;
