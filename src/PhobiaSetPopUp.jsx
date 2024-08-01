9e2d8b210d44573d471aaef8e9c870fcbb9b340c
Jovin Valdez <jovinvaldez@berkeley.edu>
2024-07-31 20:02:10 -0700

    refined home page pop up animation and styling

--- src/PhobiaSetPopUp.jsx
+++ src/PhobiaSetPopUp.jsx
@@ -1,24 +1,30 @@
 // src/PopUp.jsx
 import React from 'react';
 import './PhobiaSetPopUp.css';
+import ToggleButton from './ToggleButton.jsx';
 
 const PhobiaSetPopUp = ({ isVisible, onClose }) => {
   return (
-    <div className={`popup-overlay ${isVisible ? 'visible' : ''}`} onClick={onClose}>
-      <div className="popup-content" onClick={(e) => e.stopPropagation()}>
-        <h2>Pop-Up Content</h2>
-        <p>This is the content of the pop-up.</p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-      </div>
+    <div className="popup">
+        <div className={`popup-overlay ${isVisible ? 'visible' : ''}`} onClick={onClose}>
+            <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+                <h2>Common Phobia Triggers</h2>
+                <div className="phobia-toggles-popup">
+                    <ToggleButton className="phobia-toggle-btn">spiders</ToggleButton>
+                    <ToggleButton className="phobia-toggle-btn">snakes</ToggleButton>
+                    <ToggleButton className="phobia-toggle-btn">blood</ToggleButton>
+                    <ToggleButton className="phobia-toggle-btn">heights</ToggleButton>
+                    <ToggleButton className="phobia-toggle-btn">flying</ToggleButton>
+                </div>
+
+                <h2>Your Custom Phobia Triggers</h2>
+                <div className="custom-phobia-btns">
+                    <ToggleButton className="phobia-toggle-btn">holes/small patterns</ToggleButton>
+                    
+                </div>
+
+            </div>
+        </div>
     </div>
   );
 };
