74f1f0abba6a23daac7af66b925cba0674d15bd6
Raymond <raymond@Raymondde.local>
2024-08-02 16:23:43 -0700

    added css for customtriggers

--- src/CustomTriggers.jsx
+++ src/CustomTriggers.jsx
@@ -1,35 +1,53 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+import Button from "./Button";
+import "./CustomTriggers.css";
 
 export default function CustomTriggers() {
   const navigate = useNavigate();
 
   const goToAddCustomTrigger = () => {
-    navigate('/AddCustomTrigger')
-  }
+    navigate("/AddCustomTrigger");
+  };
 
   const goToEditCustomTrigger = () => {
-    navigate('/EditCustomTrigger')
-  }
+    navigate("/EditCustomTrigger");
+  };
 
-  
   const goToHome = () => {
-    navigate('/')
-  }
+    navigate("/");
+  };
 
   return (
-    <>
-      <button className="back-button" onClick={goToHome}>&#8592;</button>
+    <div className="custom-triggers">
+      <Button className="back-btn" onClick={goToHome}>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/566/566002.png"
+          style={{ width: "20px", height: "20px" }}
+        />
+      </Button>
 
       <h1>Custom Triggers</h1>
       <ul className="trigger-list">
         <li class="list-trigger-item">
-          <label>Holes/small patterns</label>
-          <button onClick={goToEditCustomTrigger}>🖊️</button>
+          <div className="trigger-item-text">
+            <div className="trigger-item-name">Holes/small patterns</div>
+            <div className="trigger-item-description">
+              deep fear of holes or small patterns, including cartoon depictions
+            </div>
+          </div>
+          <button
+            className="edit-custom-trigger-btn"
+            onClick={goToEditCustomTrigger}
+          >
+            🖊️
+          </button>
         </li>
       </ul>
-      <button onClick={goToAddCustomTrigger}>+ New Trigger</button>
-    </>
+      <button className="add-custom-trigger-btn" onClick={goToAddCustomTrigger}>
+        <span className="circle-icon">+</span>
+        <span className="button-text">New Trigger</span>
+      </button>
+    </div>
   );
 }
